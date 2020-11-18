1b12bdf996691880181e631300ac84ddb1d80eec
Kevin Gómez <kevingomez.a98@gmail.com>
2020-11-18 17:46:12 +0100

    initial commit

---
+++ src/main/java/com/kevingomez/FYCBackEnd/FycBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.kevingomez.FYCBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FycBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FycBackEndApplication.class, args);
+	}
+
+}
