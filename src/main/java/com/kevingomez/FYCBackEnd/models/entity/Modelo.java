2626faf0e6803f248af02125781351ee81c767f8
Kevin Gómez <kevingomez.a98@gmail.com>
2020-11-19 22:54:03 +0100

    Added Modelo entity
    Added Modelo entity

--- src/main/java/com/kevingomez/FYCBackEnd/models/entity/Modelo.java
+++ src/main/java/com/kevingomez/FYCBackEnd/models/entity/Modelo.java
@@ -1,4 +1,47 @@
 package com.kevingomez.FYCBackEnd.models.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "modelos")
 public class Modelo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "modelo_id")
+    private int idModelo;
+
+    @ManyToOne(fetch = FetchType.LAZY) // Muchos modelos pueden tener una marca
+    @JoinColumn(name = "marca_id")
+    @NotNull(message = "La marca no puede estar vacia")
+    @JsonIgnoreProperties(value={"modelo","hibernateLazyInitializer","handler"}, allowSetters = true) // Esto se hace por el fecth lazy
+    private Marca marca;
+
+    private String modelo;
+
+    public int getIdModelo() {
+        return idModelo;
+    }
+
+    public void setIdModelo(int idModelo) {
+        this.idModelo = idModelo;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 }
